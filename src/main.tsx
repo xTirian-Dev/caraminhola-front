a65601d8fee65fe1788c48d8f8b2d166883dd6be
xTirian <tirianx@yahoo.com>
2024-04-27 21:03:18 -0300

    fix Type mime main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import ReactModal from 'react-modal';
 
-const rootElement = document.getElementById('root')!;
-const root = ReactDOM.createRoot(rootElement);
-
-ReactModal.setAppElement(rootElement);
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
